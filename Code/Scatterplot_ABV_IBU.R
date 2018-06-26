678469e77339c8c0f0dd0c24f33e78c883664b05
Rebecca Holsapple <rlholsapple@gmail.com>
2018-06-26 15:56:08 -0400

    update header

--- Code/Scatterplot_ABV_IBU.R
+++ Code/Scatterplot_ABV_IBU.R
@@ -1,16 +1,16 @@
 ###########################################################################################
-# R code to Create a Scatterplot comparing Alcohol Content and Bitterness of Beer
+# R code to Create a Scatterplot Comparing Alcohol Content and Bitterness of Beer
 ## Author : Rebecca Holsapple
 ## Created Date : 06/24/2018
-## This script uses the source file breweries.csv  to determine the number of breweries  
-## in each US state and to generate a barchart of brewery count by state.
+## This script creates a scatterplot of Beer IBU and ABV
+##
 ###########################################################################################
 
 ## @knitr scatterplotabvibu
 
 # Scatterplot of IBU vs ABV Percent, with Customer Median Reference Point (red), and a linar model and confidence interval
 
-IBU.ABV.relation <- ggplot(beer.breweries.state.clean, aes(beer.breweries.state.clean$ABV, beer.breweries.state.clean$IBU)) +
+ggplot(beer.breweries.state.clean, aes(beer.breweries.state.clean$ABV, beer.breweries.state.clean$IBU)) +
   geom_point(shape=18, color="maroon4")+
   geom_point(aes(x= .0825, y = 32.5), color="darkblue", size = 5)+
   geom_smooth(method=lm,  linetype="dashed", color="forestgreen", fill="darkblue") +
